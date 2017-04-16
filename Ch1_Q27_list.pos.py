7e52bad69fbd941a4d52232b0a5c506aafba3c5a
Inshaal Uddin <inshaal@users.noreply.github.com>
2017-04-16 21:16:32 +0530

    Update Ch1_Q27_list.pos.py
    -used built-in function to insert elements which is way faster.
    -easy to understand.

--- Ch1_Q27_list.pos.py
+++ Ch1_Q27_list.pos.py
@@ -1,7 +1,9 @@
-#made by GM Harshvardhan
+#Made by Inshaal
 #To input a particular numnber in a particular position in list
 lst=input("Enter list : ")
 enter=input("Enter the digit to be input : ")
 pos=input("Enter position after which it will be added : ")
-newlst=lst[:pos] + [enter] + lst[pos:]
-print newlst
+lst.insert(pos, enter) #We can directly use the built-in function
+print lst
+'''newlst=lst[:pos] + [enter] + lst[pos:] #this was GM's idea.
+print newlst''
