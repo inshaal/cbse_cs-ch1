c04f244efa5145f5f9041671d5a6d8cba15b25f1
Inshaal Uddin <inshaal@users.noreply.github.com>
2017-04-16 11:06:46 +0530

    fixed indentation
    fixed indentation on few major code loop lines.

--- Ch1_Q14_decimal2binary.py
+++ Ch1_Q14_decimal2binary.py
@@ -11,10 +11,10 @@ l=e.split(".") #creates a list that contains integer part as 0th index and fract
 i=qi=int(l[0])#integer part
 ri=''
 while qi<>0: 
-   r=qi%2
-   qi=qi/2 
-   ri+=str(r) #adding the remainder to string
-   ip=ri[::-1] #reversing the string as the output we require will be in reversed form
+    r=qi%2
+    qi=qi/2 
+    ri+=str(r) #adding the remainder to string
+    ip=ri[::-1] #reversing the string as the output we require will be in reversed form
 
 #now comes the fraction part
 
@@ -23,15 +23,15 @@ rf=''
 frac=a%1 #value of decimal part as float
 
 while frac<>1:
-   frac=(frac%1)*2
-   x=int(frac)
-   rf+=str(x)
+    frac=(frac%1)*2
+    x=int(frac)
+    rf+=str(x)
 
 #if integer or fraction parts are 0, then a 0 will be there instead of empty string
 if i==0:
-   ri='0'
+    ri='0'
 if f==0:
-   rf='0'
+    rf='0'
 
 p=ri+"."+rf
 print p
