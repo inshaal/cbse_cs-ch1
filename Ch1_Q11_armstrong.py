48c88ce839703e74d6d85b680733cd39c69bc63e
DivyanshDC <Divyanshchhablani00@gmail.com>
2017-04-16 18:04:22 +0530

    Update Ch1_Q11_armstrong.py
    -now valid for numbers of length more/less than 3 digits

--- Ch1_Q11_armstrong.py
+++ Ch1_Q11_armstrong.py
@@ -1,10 +1,11 @@
 #To check if a number is armstrong number.
 x=input("Enter any no. ")
+lth=len(str(x)) #length of the number to be taken as power
 k=x
 sum=0
 while k>0:
     j=k%10    #this takes out the last digit.
-    sum=sum+(j**3)
+    sum=sum+(j**lth)
     k=k/10    #this removes the last digit
 if sum==x:
     print "It IS an Armstrong number. "
